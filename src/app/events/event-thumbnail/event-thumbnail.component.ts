01299ca943d2b36b4184eb55e6be586e0e2204f7
Jorge Gonçalves <jorge.a.goncalves@capgemini.com>
2022-01-27 11:56:56 -0300

    Add specific style in EventThumbnailComponent

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -1,12 +1,9 @@
 import { Component, EventEmitter, Input, Output } from "@angular/core";
 @Component({
     selector: 'event-thumbnail',
-    templateUrl: 'event-thumbnail.component.html'
+    templateUrl: 'event-thumbnail.component.html',
+    styleUrls: ["event-thumbnail.component.css"]
 })
 export class EventThumbnailComponent {
     @Input() event: any;
-
-    logFoo(){
-        console.log("Logfoo");
-    }
 }
