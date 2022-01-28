66dfdf2fc0284f25195f6bb0d871fc0b362a3538
Jorge Gonçalves <jorge.a.goncalves@capgemini.com>
2022-01-28 20:13:56 -0300

    Add ngClass by object, string or array

--- src/app/events/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail.component.ts
@@ -6,4 +6,20 @@ import { Component, EventEmitter, Input, Output } from "@angular/core";
 })
 export class EventThumbnailComponent {
     @Input() event: any;
+
+    getStartTimeClassByObject(){
+        const isEarlyStart:boolean = this.event?.time === '8:00 am';
+        return {green: isEarlyStart, bold: isEarlyStart};
+    }
+
+    
+    getStartTimeClassByString(){
+        if(this.event?.time === '8:00 am')
+            return "green bold";
+    }
+
+    getStartTimeClassByArray(){
+        if(this.event?.time === '8:00 am')
+            return ["green", "bold"];
+    }
 }
